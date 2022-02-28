dc82692ddb12eea085f7edd2c1a1c015bf5449d2
Zhiyu Zhang <zzhang.qppi@gmail.com>
2022-03-01 00:20:48 +0800

    Update deepNN.py

--- deepNN.py
+++ deepNN.py
@@ -1,78 +1,119 @@
-from tkinter import N, W, Y
 import numpy as np
-from dnn_utils import sigmoid_backward
-import lr_utils
+import matplotlib.pyplot as plt
 
-train_set_x_orig , train_set_y , test_set_x_orig , test_set_y , classes = lr_utils.load_dataset()
-hyper_para = {'l_rate':0.01, 'depth':10, 'nodes':[], 'activation_func':'sigmoid', 'output_func':'sigmoid'}
-
-assert(len(hyper_para['nodes']) == hyper_para['depth'])
-
-def initialize(nodes, depth):
+def initialize_parameters(n_dimensions_f):
+	#n_dimensions_f是一个包含每一层节点数的数组（包括输入层和输出层）
+	L = len(n_dimensions_f)-1
 	parameters = {}
-	assert(depth == len(nodes))
-	for i in range(1, depth+1):
-		parameters['w%d'%i] = np.random.randn(nodes[i], nodes[i-1])
-		parameters['b%d'%i] = np.random.randn(nodes[i], train_set_x_orig.shape[0])
+	for i in range(1, L+1):
+		parameters['W%d'%i] = np.random.randn(n_dimensions_f[i], n_dimensions_f[i-1]) * 0.01
+		parameters['b%d'%i] = np.zeros((n_dimensions_f[i], 1))
 	return parameters
 
-def linear_forward(A, W, b):
-	Z = np.dot(W, A) + b
-	cache = [A, W, b]
-	return Z
-#	return Z, cache
-
-def activation_forward(A_prev, W, b, activation_func):
+def forward_propogation(A_prev, W, b, activation_func):
 	if activation_func == 'sigmoid':
-#		Z, linear_cache = linear_forward(A_prev, W, b)
-		Z = linear_forward(A_prev, W, b)
+		Z = np.dot(W, A_prev) + b
 		A = 1/(1+np.exp(-Z))
 	elif activation_func == 'relu':
-		Z = linear_forward(A_prev, W, b)
+		Z = np.dot(W, A_prev) + b
 		A = np.maximum(0, Z)
-#	elif activation_func == 'tanh':
-	return A
+	linear_cache = (A_prev, W, b)
+	activation_cache = Z
+	cache = (linear_cache, activation_cache)
+	return A, cache
 
-def forward_prop(A, parameters, depth, activation_func, output_func):
-	for i in range(1, depth):
-		A = activation_forward(A, parameters['w%d'%i], parameters['b%d'%i], activation_func)
-	A_output = activation_forward(A, parameters['w%d'%depth], parameters['b%d'%depth], output_func)
-	return A_output
+def forward_in_total(A0, parameters):
+	caches = []
+	A = A0
+	L = len(parameters)/2
+	for i in range(1, L):
+		A_prev = A
+		W = parameters['W%d'%i]
+		b = parameters['b%d'%i]
+		A, cache = forward_propogation(A_prev, W, b, activation_func='sigmoid')
+		caches.append(cache)
+	A_output, cache = forward_propogation(A, 'W%d'%L, 'b%d'%L)
+	caches.append(cache)
+	assert A_output.shape == (1,A0.shape[1])
+	assert len(caches) == L
+	return A_output, caches
 
-def cost(A, Y):
+def cost_calculation(A_output, Y):
 	m = Y.shape[1]
-	cost = -np.sum(np.multiply(np.log(A),Y) + np.multiply(np.log(1 - A), 1 - Y))/m
-#	cost = np.squeeze(cost)
+	cost = -np.sum(np.multiply(np.log(A_output),Y) + np.multiply(np.log(1 - A_output), 1 - Y))/m
 	return cost
 
-def activation_backward(dA, Z, activation_func):
-	if activation_func == 'sigmoid':
-		s = 1/(1+np.exp(-Z))
-		dZ = dA * s * (1-s)
-	elif activation_func == 'relu':
-#		Z[Z>0] = 1
-#		dZ = Z
-		dZ = np.array(dA, copy=True)
-		dZ[Z <= 0] = 0
+def sigmoid_backward(dA, Z):
+	a = 1/(1+np.exp(-Z))
+	dZ = dA * a * (1-a)
 	return dZ
-#	elif activation_func = 'tanh':
 
-def linear_backward(dZ, A_prev, W):
-	dA_prev = np.dot(W.T, dZ)
+def relu_backward(dA, Z):
+	dZ = np.array(dA, copy=True)
+	dZ[Z <= 0] = 0
+	return dZ
+
+def linear_backward(dZ, linear_cache):
+	A_prev, W, b = linear_cache
 	m = A_prev.shape[1]
-	dW = np.dot(dZ, A_prev.T)/m
-	db = np.sum(dZ, axis=1, keepdims=True)/m
+	dW = np.dot(A_prev.T, dZ) / m
+	db = np.sum(dZ, axis=1, keepdims=True) / m
+	dA_prev = np.dot(W.T, dZ)
 	return dA_prev, dW, db
 
-def linear_activation_backward(dA, A_prev, Z, activation_func):
-	dZ = activation_backward(dA, Z, activation_func)
-	dA_prev, dW, db = linear_backward(dZ, A_prev, W)
+def linear_activation_backward(dA, linear_cache, activation_cache, activation_func):
+	if activation_func == 'sigmoid':
+		dZ = sigmoid_backward(dA, activation_cache)
+		dA_prev, dW, db = linear_backward(dZ, linear_cache)
+	elif activation_func == 'relu':
+		dZ = relu_backward(dA, activation_cache)
+		dA_prev, dW, db = linear_backward(dZ, linear_cache)
 	return dA_prev, dW, db
 
+def backward_in_total(A_output, train_Y_f, caches):
+	gradients = {}
+	dA_output = - (np.divide(train_Y_f, A_output) - np.divide(1 - train_Y_f, 1 - A_output))
+	dA = dA_output
+	for i in range(1, len(caches)+1):
+		dA = dA_prev
+		linear_cache, activaiton_cache = caches[len(caches)-i]
+		dA_prev, dW, db = linear_activation_backward(dA, linear_cache, activaiton_cache)
+		gradients['dA%d'%(len(caches)+1-i)]=dA_prev
+		gradients['dW%d'%(len(caches)+1-i)]=dW
+		gradients['db%d'%(len(caches)+1-i)]=db
+	return gradients
 
-def gradient_descent(W, dW, b, db, l_rate):
-	W, b = W - l_rate*dW, b - l_rate*db
-	return W, b
+def update_parameters(parameters, gradients, l_rate_f):
+	L = len(parameters)/2
+	for i in range(1, L+1):
+		parameters['w%d'%i] = parameters['w%d'%i] - l_rate_f * gradients['w%d'%i]
+		parameters['b%d'%i] = parameters['b%d'%i] - l_rate_f * gradients['b%d'%i]
+	return parameters
 
-if __name__ == '__main__':
+def multi_layers_learning(train_X_f, train_Y_f, n_dimensions_f, l_rate_f, n_iterations_f):
+	parameters = initialize_parameters(n_dimensions_f)
+	costs = []
+	for i in range(n_iterations_f):
+		A_output, caches = forward_in_total(train_X_f, parameters)
+		cost = cost_calculation(A_output, train_Y_f)
+		costs.append(int(cost))
+		gradients = backward_in_total(A_output, train_Y_f, caches)
+		parameters = update_parameters(parameters, gradients, l_rate_f)
+	costs_plot, ax = plt.subplots()
+	ax.plot(range(len(costs)), costs, linewidth=2.0)
+	ax.set(xlable='iterations', ylable='costs', title='costs_plot')
+	plt.show()
+	return parameters
 
+def test(test_X_f, test_Y_f, parameters):
+	A_output, caches = forward_in_total(test_X_f, parameters)
+	A_output[A_output>=0.5]=1
+	A_output[A_output<0.5]=1
+	assert(A_output.shape == test_Y_f.shape)
+	compare_result = (A_output == test_Y_f)
+	accuracy = np.sum(compare_result)/test_X_f.shape[1]
+	return accuracy
+
+
+parameters = multi_layers_learning(train_X, train_Y, n_dimensions, l_rate, n_iterations)
+print('模型准确度：' + test(test_X, test_Y, parameters))
